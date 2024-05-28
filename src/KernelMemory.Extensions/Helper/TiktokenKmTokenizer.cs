2374c860c6a456466299898711106779a433fe76
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2024-05-28 11:28:48 +0200

    Added ML tokenizer

---
+++ src/KernelMemory.Extensions/Helper/TiktokenKmTokenizer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.KernelMemory.AI;
+using Microsoft.ML.Tokenizers;
+using System;
+
+namespace KernelMemory.Extensions.Helper
+{
+    /// <summary>
+    /// This text tokenizer uses latest and optimized Tiktoken model directly
+    /// from Microsoft packages that offer optimized countTokens method.
+    /// </summary>
+#pragma warning disable KMEXP00 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+    public class TiktokenKmTokenizer : ITextTokenizer
+#pragma warning restore KMEXP00 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+    {
+        private Tokenizer _tikToken;
+
+        public TiktokenKmTokenizer(string baseModelName)
+        {
+            _tikToken = Tiktoken.CreateTiktokenForModel(baseModelName);
+        }
+
+        public int CountTokens(string text)
+        {
+            return _tikToken.CountTokens(text);
+        }
+    }
+}
